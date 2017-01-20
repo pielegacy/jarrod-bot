1c588de38f37cd5908b1b5ca47f004ac5449cffd
pielegacy <alex_billson@outlook.com>
2017-01-20 21:21:54 +1100

    Quote ability

--- index.js
+++ index.js
@@ -6,31 +6,13 @@ var token = "";
 var quoteJson;
 client.on('ready', function () {
     console.log("It's a me, Andrew! Call me by typing Italiano");
-    client.user.setGame("with myself");
-    // http.get("http://ripperquotes.azurewebsites.net/api/QuotesApi", (res) => {
-    //     let dataString = "";
-    //     res.on('data', (d) => dataString += d);
-    //     res.on('end', () => quoteJson = JSON.parse(dataString));
-    // });
-    // client.channels.get("name", "reddit-free-zone");
+    client.user.setGame("Minecraft");
+    http.get("http://ripperquotes.azurewebsites.net/api/QuotesApi", function (res) {
+        var dataString = "";
+        res.on('data', function (d) { return dataString += d; });
+        res.on('end', function () { return quoteJson = JSON.parse(dataString); });
+    });
 });
-var sayings = [
-    "Mamma mia!",
-    "Wowee, I love Anime and Pizza!",
-    "Yahoo! Italy!",
-    "https://barbaracartategui.files.wordpress.com/2010/11/pizza-pepperoni.jpg",
-    "http://www.ezilon.com/maps/images/europe/Italian-road-map.gif",
-    "Videogames my man!",
-    "Weed my dude!",
-    "You found Dark Souls hard? Well gee get good",
-    "Metal music and Vaporwave, that's the stuff",
-    "http://4chan.org/ck is where it's at",
-    ":pizza:",
-    "My Dad's dead",
-    "No shazz I wasn't bonging on",
-    "I didn't think I was this quotable",
-    "Drake's probably the whitest black guy out there"
-];
 var ThoughtsRead = function () {
     var thoughtString = fs.readFileSync("thoughts.json");
     var thoughts = thoughtString == undefined ? [] : JSON.parse(thoughtString);
@@ -40,7 +22,7 @@ var ThoughtsSave = function (thoughts) {
     fs.writeFileSync("thoughts.json", JSON.stringify(thoughts));
 };
 client.on('message', function (message) {
-    if (message.content.startsWith("Italiano")) {
+    if (message.content.startsWith("Milky")) {
         if (message.content.search(" remember ") != -1) {
             var thought = message.content.split(" remember ")[1];
             if (thought != "") {
@@ -64,9 +46,8 @@ client.on('message', function (message) {
                 message.reply("I don't know anything, tell me to remember something");
         }
         else {
-            var index = Math.floor(Math.random() * (sayings.length));
-            var response = sayings[index];
-            message.reply(response);
+            var quote = quoteJson[Math.floor(Math.random() * (quoteJson.length))];
+            message.reply("\"" + quote.QuoteText + "\"\n \t~ " + quote.QuoteAuthor);
         }
     }
 });
--- index.ts
+++ index.ts
@@ -6,31 +6,13 @@ let token: string = "";
 let quoteJson;
 client.on('ready', () => {
     console.log("It's a me, Andrew! Call me by typing Italiano");
-    client.user.setGame("with myself");
-    // http.get("http://ripperquotes.azurewebsites.net/api/QuotesApi", (res) => {
-    //     let dataString = "";
-    //     res.on('data', (d) => dataString += d);
-    //     res.on('end', () => quoteJson = JSON.parse(dataString));
-    // });
-    // client.channels.get("name", "reddit-free-zone");
+    client.user.setGame("Minecraft");
+    http.get("http://ripperquotes.azurewebsites.net/api/QuotesApi", (res) => {
+        let dataString = "";
+        res.on('data', (d) => dataString += d);
+        res.on('end', () => quoteJson = JSON.parse(dataString));
+    });
 });
-const sayings: Array<string> = [
-    "Mamma mia!",
-    "Wowee, I love Anime and Pizza!",
-    "Yahoo! Italy!",
-    "https://barbaracartategui.files.wordpress.com/2010/11/pizza-pepperoni.jpg",
-    "http://www.ezilon.com/maps/images/europe/Italian-road-map.gif",
-    "Videogames my man!",
-    "Weed my dude!",
-    "You found Dark Souls hard? Well gee get good",
-    "Metal music and Vaporwave, that's the stuff",
-    "http://4chan.org/ck is where it's at",
-    ":pizza:",
-    "My Dad's dead",
-    "No shazz I wasn't bonging on",
-    "I didn't think I was this quotable",
-    "Drake's probably the whitest black guy out there"
-];
 const ThoughtsRead = (): Array<string> => {
     let thoughtString = fs.readFileSync("thoughts.json");
     let thoughts = thoughtString == undefined ? ([] as Array<string>) : JSON.parse(thoughtString);
@@ -40,7 +22,7 @@ const ThoughtsSave = (thoughts: Array<string>) => {
     fs.writeFileSync("thoughts.json", JSON.stringify(thoughts));
 }
 client.on('message', message => {
-    if (message.content.startsWith("Italiano")) {
+    if (message.content.startsWith("Milky")) {
         if ((message.content as string).search(" remember ") != -1) {
             let thought = (message.content as string).split(" remember ")[1];
             if (thought != "") {
@@ -63,14 +45,9 @@ client.on('message', message => {
             else
                 message.reply("I don't know anything, tell me to remember something");
         }
-        // else if ((message.content as string).search("quote") != -1){
-        //     let quote = quoteJson[Math.floor(Math.random() * (quoteJson.length))];
-        //     message.reply(`"${quote.QuoteText}"\n \t~ ${quote.QuoteAuthor}`);
-        // }
         else {
-            let index = Math.floor(Math.random() * (sayings.length));
-            let response = sayings[index];
-            message.reply(response);
+            let quote = quoteJson[Math.floor(Math.random() * (quoteJson.length))];
+            message.reply(`"${quote.QuoteText}"\n \t~ ${quote.QuoteAuthor}`);
         }
     }
 });
